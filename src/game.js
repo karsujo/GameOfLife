9289159a5cbd2fb59506131b9d395ae70f562037
karthik <karsujo@gmail.com>
2020-08-25 06:03:38 -0700

    Reorg

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
-//Todo track entity selection/spwn/kill based upon GUI visz\
+//Comments : 
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
@@ -17,7 +17,9 @@ class entity {
 
 initializeBoard();
 
-function startGame() { setInterval(nextGen(context), 100) };
+function startGame() {
+    setInterval(nextGen, 1000);
+};
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -41,21 +43,47 @@ function IsEntityWhite(context, x, y) {
 function initializeBoard() {
     context.fillStyle = 'gray';
     context.fillRect(0, 0, canvas.width, canvas.height);
-    let seedEntity = new entity();
+    //let seedEntity = new entity();
     // seedEntity.x = Math.floor(Math.random() * (8 - 1) + 1)*boxUnit;
     // seedEntity.y = Math.floor(Math.random() * (8 - 1) + 1*boxUnit);
 
-    seedEntity.x = 64;
-    seedEntity.y = 128;
-    seedEntity.state = 'alive';
-    spawnEntity(seedEntity, context);
+    // seedEntity.x = 64;
+    // seedEntity.y = 128;
+    // seedEntity.state = 'alive';
+    // spawnEntity(seedEntity, context);
+
+    initializeEntities();
+}
+
+function initializeEntities() {
+    let e1 = new entity();
+    e1.x = 64;
+    e1.y = 128;
+    spawnEntity(e1, context);
+
+    let e2 = new entity();
+    e2.x = 80;
+    e2.y = 136;
+    spawnEntity(e2, context);
+
+
+    let e3 = new entity();
+    e3.x = 64;
+    e3.y = 128 + 8;
+    spawnEntity(e3, context);
+
+    let e4 = new entity();
+    e4.x = 64 + 8;
+    e4.y = 128 + 8;
+    spawnEntity(e4, context);
+
 }
 
 function createEntity(x, y, state) {
     let currentEntity = new entity();
     currentEntity.x = x;
     currentEntity.y = y;
-    currentEntity.state = state;
+    currentEntity.state = IsEntityWhite(context, x, y) == true ? 'alive' : 'dead';
     return currentEntity;
 }
 
@@ -65,49 +93,87 @@ function trackEntity(x, y) {
     ctxt.fillRect(x, y, boxUnit, boxUnit);
 }
 
-function computeNeighborValues(context, x, y) {
-    var values = new Array();
-    var entities = new Array();
+function computeNeighborValues(x, y) {
+    var neighborMatrix = new Array();
+    //  ________
+    // |X||_||_|
+    // |_||_||_|
+    // |_||_||_|
     var xpos = x - boxUnit;
     var ypos = y + boxUnit;
-    if (xpos > 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-
-        entities[0] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[0] = createEntity(xpos, ypos);
     }
+    //  ________
+    // |_||X||_|
+    // |_||_||_|
+    // |_||_||_|
     (xpos = x), (ypos = y + boxUnit);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[1] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[1] = createEntity(xpos, ypos);
     }
+    //  ________
+    // |_||_||X|
+    // |_||_||_|
+    // |_||_||_|
     (xpos = x + boxUnit), (ypos = y + boxUnit);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[2] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[2] = createEntity(xpos, ypos);
     }
+    //  ________
+    // |_||_||_|
+    // |_||_||X|
+    // |_||_||_|
     (xpos = x + boxUnit), (ypos = y);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[3] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[3] = createEntity(xpos, ypos);
     }
+    //  ________
+    // |_||_||_|
+    // |_||_||_|
+    // |_||_||X|
     (xpos = x - boxUnit), (ypos = y + boxUnit);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[4] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[4] = createEntity(xpos, ypos);
     }
+    //  ________
+    // |_||_||_|
+    // |_||_||_|
+    // |_||X||_|
     (xpos = x), (ypos = y - boxUnit);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[5] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[5] = createEntity(xpos, ypos);
     }
+    //  ________
+    // |_||_||_|
+    // |_||_||_|
+    // |X||_||_|
     (xpos = x - boxUnit), (ypos = y - boxUnit);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[6] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[6] = createEntity(xpos, ypos);
     }
+    //  ________
+    // |_||_||_|
+    // |X||_||_|
+    // |_||_||_|
     (xpos = x - boxUnit), (ypos = y);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[7] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[7] = createEntity(xpos, ypos);
     }
     //current cell
+    //  ________
+    // |_||_||_|
+    // |_||X||_|
+    // |_||_||_|
     (xpos = x), (ypos = y);
-    if (xpos >= 0 && ypos > 0 && xpos < canvas.width && ypos < canvas.height) {
-        entities[8] = createEntity(xpos, ypos, IsEntityWhite(context, xpos, ypos) == true ? 'alive' : 'dead');
+    if (IsWithinGrid(xpos, ypos)) {
+        neighborMatrix[8] = createEntity(xpos, ypos);
     }
-    return entities;
+    return neighborMatrix;
+}
+
+function IsWithinGrid(x, y) {
+    return (x >= 0 && y > 0 && x < canvas.width && y < canvas.height);
 }
 
 function getNeighborLiveCount(entities) {
@@ -124,7 +190,7 @@ function getNeighborLiveCount(entities) {
 
 function killEntity(entity) {
     let ctxt = canvas.getContext('2d');
-    ctxt.fillStyle = 'red';
+    ctxt.fillStyle = 'gray';
     ctxt.fillRect(entity.x, entity.y, boxUnit, boxUnit);
 }
 
@@ -134,38 +200,45 @@ function spawnEntity(entity) {
     ctxt.fillRect(entity.x, entity.y, boxUnit, boxUnit);
 }
 
-function computeRules(entities, context) {
+function computeRules(entities) {
     if (entities.length >= 8) {
         let currentEntity = entities[8];
         let neighborLiveCount = getNeighborLiveCount(entities);
+        if (currentEntity.state == 'alive') {
+            neighborLiveCount--; //Ignore current cell from neighnor count.
+        }
+
         //Rule: live cell whose neighbor live cell count <2 -> die
         if (currentEntity.state == 'alive' && neighborLiveCount < 2) {
             //kill entity
-            killEntity(currentEntity, context)
+            killEntity(currentEntity)
         }
         if (currentEntity.state == 'alive' && (neighborLiveCount == 2 || neighborLiveCount == 3)) {
             //remain alive
         }
         if (currentEntity.state == 'alive' && neighborLiveCount > 3) {
             //kill entity
-            killEntity(currentEntity, context)
+            killEntity(currentEntity)
         }
         if (currentEntity.state == 'dead' && neighborLiveCount == 3) {
             //entity born
-            spawnEntity(currentEntity, context);
+            spawnEntity(currentEntity);
         }
     }
 }
 
-function nextGen(context) {
+function go() {}
+
+
+function nextGen() {
     for (i = 0; i < canvas.width; i = i + boxUnit) {
         for (j = 0; j < canvas.height; j = j + boxUnit) {
-            //trackEntity(i,j);
+            // trackEntity(i, j);
             // if(i==64 && j==128)
             // {
             // 	alert("Hit");
             // }
-            computeRules(computeNeighborValues(context, i, j), context);
+            computeRules(computeNeighborValues(i, j));
         }
     }
 }
